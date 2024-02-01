b4013535f5782847447e8e1c658c8ac8dbbc6bd9
re-gis <irumvaregisdmc@gmail.com>
2024-02-01 21:41:27 +0200

    Initial commit

---
+++ products/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse("Hello world")
+
+def new(request):
+    return HttpResponse("This is a new page")
